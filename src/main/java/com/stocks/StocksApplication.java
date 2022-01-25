55235d0f28830c89e19164e631f35b179b98878e
Bbhargavi <Lakshmibhargavi1992@gmail.com>
2022-01-25 09:37:30 -0600

    changes new

--- src/main/java/com/stocks/StocksApplication.java
+++ src/main/java/com/stocks/StocksApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
 public class StocksApplication {
 
     public static void main(String[] args) {
